db64fd0bd7830fc358ff88e63f28dc40ae9bb607
Jardel Lion <127162820+JardelLion@users.noreply.github.com>
2023-09-28 02:38:16 +0200

    aA

--- AUTOMATION BET - Copia/soccer_stats/league_allowed.py
+++ AUTOMATION BET - Copia/soccer_stats/league_allowed.py
@@ -2,13 +2,13 @@ leagues = [
     {'italy':[
         'Italy - Serie A',
         'Italy - Serie B',
-        'Italy - Serie C - Group A',
-        'Italy - Serie C - Group B',
-        'Italy - Serie C - Group C',
-        'Italy - Serie D - Group A',
-        'Italy - Serie D - Group B',
-        'Italy - Serie D - Group C',
-        'Italy - Serie D - Group D'
+        'Italy - Serie C - Group A#',
+        'Italy - Serie C - Group B#',
+        'Italy - Serie C - Group C#',
+        'Italy - Serie D - Group A#',
+        'Italy - Serie D - Group B#',
+        'Italy - Serie D - Group C#',
+        'Italy - Serie D - Group D#'
         
     ],
 
@@ -20,18 +20,18 @@ leagues = [
         
     ],
     'brazil': [
-        'Brazil - Serie A',
-        'Brazil - Serie B',
-        'Brazil - Serie C',
-        'Brazil - Serie D',
-        'Brazil - Paulista A1',
-        'Brazil - Parabaino'
+        'Brazil - Serie A#',
+        'Brazil - Serie B#',
+        'Brazil - Serie C#',
+        'Brazil - Serie D#',
+        'Brazil - Paulista A1#',
+        'Brazil - Parabaino#'
     ],
     'francesa': [
         'France - Ligue 1',
         'France - Ligue 2',
-        'France - National',
-        'France - Nacional'
+        'France - National#',
+        'France - Nacional#'
         
     ],
     'inglesa': [
@@ -43,46 +43,46 @@ leagues = [
         'REMOVE ADS - Enjoy an ad-free experience on SoccerSTATS.com and show your support: become a Member!'
     ],
     'argentina': [
-        'Argentina - Primera Nacional',
-        'Argentina - Primera C',
-        'Argentina - Primera C - Apertura',
-        'Argentina - Primera C - Clausura',
-        'Argentina - Liga Profesional'
+        'Argentina - Primera Nacional#',
+        'Argentina - Primera C#',
+        'Argentina - Primera C - Apertura#',
+        'Argentina - Primera C - Clausura#',
+        'Argentina - Liga Profesional#'
         
     ],
     'japao': [
-        'Japan - J1 League',
-        'Japan - J2 League',
-        'Japan - J3 League'
+        'Japan - J1 League#',
+        'Japan - J2 League#',
+        'Japan - J3 League#'
     ],
     'korea': [
-        'South Korea - K League 1',
-        'South Korea - K League 2',
-        'South Korea - K3 League'
+        'South Korea - K League 1#',
+        'South Korea - K League 2#',
+        'South Korea - K3 League#'
     ],
     'alemanha': [
         'Bundesliga',
-        'Germany - 2. Liga'
+        'Germany - 2. Liga#'
     ],
      'liga_portuguesa': [
-         'Portugal - Primeira Liga', # e a mesma coisa que liga portugal
-         'Portugal - Liga Portugal',
-         'Portugal - Liga Portugal 2',
-         'Portugal - Segunda Liga']
+         'Portugal - Primeira Liga#', # e a mesma coisa que liga portugal
+         'Portugal - Liga Portugal#',
+         'Portugal - Liga Portugal 2#',
+         'Portugal - Segunda Liga#']
     ,
     'scotland': [
-        'Scotland - Championship'
+        'Scotland - Championship#'
     ],
 
     'africa': [
-        'South Africa - Premier Division',
-        'South Africa - First Division'
+        'South Africa - Premier Division#',
+        'South Africa - First Division#'
     ],
-    'zambia': ['zambia - super league'],
+    'zambia': ['zambia - super league#'],
     'usa': [
-        'USA - MLS',
-        'USA - USL Championship',
-        'USA - USL League One'
+        'USA - MLS#',
+        'USA - USL Championship#',
+        'USA - USL League One#'
     ]
     
     }
