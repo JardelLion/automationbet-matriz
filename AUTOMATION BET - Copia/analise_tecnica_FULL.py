3a168ba63e454d8576b2002551d034f2e290535b
Jardel Lion <127162820+JardelLion@users.noreply.github.com>
2023-09-30 01:04:56 +0200

    d

--- AUTOMATION BET - Copia/analise_tecnica_FULL.py
+++ AUTOMATION BET - Copia/analise_tecnica_FULL.py
@@ -9,7 +9,7 @@ def get_sheet_name(sheet_name):
 workbook = openpyxl.load_workbook('tecnica_analise/2023/MATRIZ-FULL-2023.xlsx')
 sheet = workbook[get_sheet_name('setembro')]
 
-question_day =  '27/9/2023' #data 10/5/2010
+question_day =  '30/9/2023' #data 10/5/2010
 #str(input("Qual é o dia que se quer analisar [10/04/2023] / [all]: "))
 
 
