f4469c7b66f06808cbdd0188f62fe8dead1b3635
Jardel Lion <businessjbx@gmail.com>
2023-11-02 07:51:47 -0200

    2/11/2023

--- AUTOMATION BET - Copia/analise_tecnica_EXPECTED_OVER.py
+++ AUTOMATION BET - Copia/analise_tecnica_EXPECTED_OVER.py
@@ -7,9 +7,9 @@ def get_sheet_name(sheet_name):
 
 
 workbook = openpyxl.load_workbook('tecnica_analise/2023/EXPECTED-OVER-2023.xlsx')
-sheet = workbook[get_sheet_name('outubro')]
+sheet = workbook[get_sheet_name('novembro')]
 
-question_day =  '28/10/2023' #data 10/5/2010
+question_day =  '1/11/2023' #data 10/5/2010
 #str(input("Qual é o dia que se quer analisar [10/04/2023] / [all]: "))
 
 
