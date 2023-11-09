a842f01c33ed5f1cc3369af3efc04c08386e2cb8
JardelLion <businessjbx@gmail.com>
2023-11-09 09:10:08 +0200

    9/11/2023

--- AUTOMATION BET - Copia/analise_fundamentalista.py
+++ AUTOMATION BET - Copia/analise_fundamentalista.py
@@ -17,7 +17,7 @@ from soccer_stats.table_result_passed import TableResultPassed
 
 
 #tHE PRINCIPAL AUTOMATION BET
-recent_paths = Path(r'C:\Users\JBXBILLIONS\Documents\BET\2023\\11-NOVEMBRO\\666').glob("**\*.html")
+recent_paths = Path(r'C:\Users\JBXBILLIONS\Documents\BET\2023\\11-NOVEMBRO\\11').glob("**\*.html")
 
 paths = []
 for path in recent_paths:
